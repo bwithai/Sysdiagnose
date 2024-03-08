467ab6ae0eeac77238d89946a8b731f932725f6e
bwithai <sanaullah41821@gmail.com>
2024-03-08 12:27:14 +0500

    add reusability and remove duplication code

--- routers/get_stats.py
+++ routers/get_stats.py
@@ -1,15 +1,18 @@
+import os
+
 from fastapi import APIRouter
 
 from exceptions import not_found_exception
 from iShutdown_KasperskyLab.iShutdown_stats import get_log_stats
-from utils import get_log_file
+from utils import get_target_file
 
 stats_router = APIRouter(prefix="", tags=["Reboot stats of a target Shutdown.log"])
 
 
 @stats_router.get("/log-stats/")
 async def get_stats():
-    logfile = get_log_file()
+    logfile_dir = os.path.join(os.getcwd(), "storage/parse_shutdown")
+    logfile = get_target_file(logfile_dir, ".log")
     if not logfile:
         raise not_found_exception
 
